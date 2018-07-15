2bfdb19edbfcee2418417650e84bd253c6d6ad10
Tysheba <tmorgan2dev@gmail.com>
2018-07-15 04:00:34 -0400

    first Mac update

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// Select a theme
+// Use key events to listen for the letters that players will type
+// Display Wins (# of times user guessed the word correctly)
+// When game starts display a mask for the number of letters in the word the player needs to guess
+// Reveal correct letters as they are guessed
+// Display number of guesses remaining
+// Display letters already guessed 
+// When user wins/loses automatically go to another word for player to guess
